97614b3ee2964603ad0f15d7ae944883791d7b21
himanshu <him9711973697@gmail.com>
2019-12-05 21:55:16 +0530

    problem 1

---
+++ problem 1/solution 1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+int n = 20;
+int A[20] ;
+int thrice;
+int sum;
+using namespace std;
+int main()
+cout<<"\nEnter the numbers in the array:-\n";
+{
+    for (int i=0;i<n;i++){
+        cin>>A[i];
+    }
+    thrice = n / 3;
+
+
+    sort(A, A + n);
+    cout << "sorted\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << "\n";
+    }
+    cout << "\nanswer array\n";
+    for (int i = 0; i < n; i++)
+    {
+        sum = 0;
+        if (A[i] != A[i + 1])
+        {
+            // cout << A[i];
+            for (int j = 0; j < n; j++)
+            {
+                if (A[i] == A[j])
+                {
+                    sum++;
+                }
+            }
+            cout << A[i] << ":" << sum << "\n";
+            if (sum > thrice)
+            {
+                cout << A[i] << "\n";
+            }
+            else
+            {
+                cout << "-1"
+                     << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
