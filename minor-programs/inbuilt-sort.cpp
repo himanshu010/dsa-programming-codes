a789c8d9010748fc556d3ba7f0252ec564f8769e
himanshu <him9711973697@gmail.com>
2019-12-28 23:35:00 +0530

    inbuilt sort library

---
+++ minor-programs/inbuilt-sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+bool mycompare(int a, int b){
+    cout <<"compare :" <<a<<"and"<<b<<"\n";
+    return b<a;
+    
+}
+int main() {
+    int a[]={1,4,7,5,4,5,6,7,3,66,5,4,55,4};
+    int n= sizeof(a)/sizeof(int);
+    sort(a,a+n,mycompare);
+    for(int i=0; i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
