e76a25606bbe0551c84359218a7b5efd9b98cafd
himanshu010 <himanshuaswal@yahoo.com>
2020-12-19 13:01:36 +0530

    new

---
+++ gfg/Merge Without Extra Space gfg.cpp
@@ -0,0 +1,77 @@
+/*
+*-----------------------------------------------------------*
+|                                                           |
+|                                                           |
+|               AUTHOR: Himanshu Aswal                      |
+|     ( website: himanshu010.github.io/Portfolio_website )  |
+|                                                           |
+|                                                           |
+*-----------------------------------------------------------*
+*/
+#include<bits/stdc++.h>
+#define moduli 998244353
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define P pair<int,int>
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vb vector<bool>
+#define um unordered_map
+using namespace std;
+
+void merge(int arr1[], int arr2[], int n, int m)
+{
+	int x = n - 1, y = 0;
+	while (x >= 0 && y < m) {
+		if (arr1[x] > arr2[y]) {
+			swap(arr1[x] , arr2[y]);
+			x--;
+			y++;
+		}
+		else {
+			x--;
+		}
+	}
+	sort(arr1 , arr1 + n);
+	sort(arr2 , arr2 + m);
+}
+
+void solve(int tc) {
+	int i, j, k, n, m, ans = 0, cnt = 0, sum = 0;
+	cin >> n >> m;
+	int a[n], b[m];
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> b[i];
+	}
+
+	merge(a, b, n, m);
+	for (int i = 0; i < m; ++i)
+	{
+		cout << b[i] << ' ';
+	}
+
+}
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base:: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int tc = 1;
+	// int t;cin>>t;while(t--)
+	{
+		solve(tc);
+		tc++;
+	}
+}
--- new.cpp
+++ new.cpp
@@ -1,24 +1,71 @@
-vector<int> common_element(vector<int>v1, vector<int>v2)
-{
-	sort(v1.begin(), v1.end());
-	sort(v2.begin(), v2.end());
-	int n = v1.size();
-	int m = v2.size();
-	vector<int> ans;
+/*
+*-----------------------------------------------------------*
+|                                                           |
+|                                                           |
+|               AUTHOR: Himanshu Aswal                      |
+|     ( website: himanshu010.github.io/Portfolio_website )  |
+|                                                           |
+|                                                           |
+*-----------------------------------------------------------*
+*/
+#include<bits/stdc++.h>
+#define moduli 998244353
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define P pair<int,int>
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vb vector<bool>
+#define um unordered_map
+using namespace std;
 
-	int i = 0; int j = 0;
-	while (i < n and j < m) {
-		if (v1[i] == v2[j]) {
-			ans.push_back(v1[i]);
-			i++;
-			j++;
-		}
-		else if (v1[i] < v2[j]) {
-			i++;
-		}
-		else {
-			j++;
-		}
+int findMinCost(vi a, int n) {
+	int sum = 0;
+	int mid;
+	if (n & 1) {
+		mid = a[n / 2];
 	}
-	return ans;
+	else {
+		mid = (a[n / 2] + a[n / 2 - 1]) / 2;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		sum += abs(mid - a[i]);
+	}
+	return sum;
+}
+
+void solve(int tc) {
+	int i, j, k, n, m, ans = 0, cnt = 0, sum = 0;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+
+	cout << findMinCost(a, n) << endl;
+
+}
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base:: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int tc = 1;
+	// int t;cin>>t;while(t--)
+	{
+		solve(tc);
+		tc++;
+	}
 }
