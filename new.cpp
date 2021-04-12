024f71c1f01b6cd8e64d68b0fa4f8b60b06e7eca
Your Name <you@example.com>
2021-04-12 23:25:51 +0530

    new

--- new.cpp
+++ new.cpp
@@ -1,36 +1,80 @@
-#include <bits/stdc++.h>
+/*
+*-----------------------------------------------------------*
+|                                                           |
+|                                                           |
+|               AUTHOR: Himanshu Aswal                      |
+|            ( website: himanshuaswal.codes )               |
+|                                                           |
+|                                                           |
+*-----------------------------------------------------------*
+*/
+#include<bits/stdc++.h>
+#define moduli 998244353
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define P pair<int,int>
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vb vector<bool>
+#define um unordered_map
+#define PQ priority_queue
 using namespace std;
 
-int findAllWays(int n, int m)
-{
-    if (n < m)
+int quickSortHelper(vi &a, int s, int e) {
+    int p = e;
+    int n = a.size();
+    int j = s;
+    for (int  i = s; i < e; ++i)
     {
-        return 1;
-    }
-    vector<int> dp(n + 1);
-    for (int i = 0; i < m; ++i)
-    {
-        dp[i] = 1;
-    }
-    for (int i = m; i <= n; i++)
-    {
-        dp[i] = dp[i - 1] + dp[i - m];
+        if (a[i] < a[p]) {
+            swap(a[i], a[j]);
+            j++;
+        }
     }
 
-    // if (n - 2 < 0) {
-    //     return 1;
-    // }
-
-    // return findAllWays(n-1)+findAllWays(n-2);
-    return dp[n];
+    swap(a[e], a[j]);
+    return j;
 }
 
-int main()
-{
-    int n, m;
+void quickSort(vi &a, int s, int e) {
+    if (s >= e) {
+        return;
+    }
+    int p = quickSortHelper(a, s, e);
+    // int p = s + 1;
+
+    quickSort(a, s, p - 1);
+    quickSort(a, p + 1, e);
+}
+
+
+void solve(int tc) {
+    int i, j, k, n, m, ans = 0, cnt = 0, sum = 0;
     cin >> n;
-    // if (n > m) {
-    //     swap(n, m);
-    // }
-    cout << n << endl;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+
+    quickSort(a, 0, n - 1);
+    for (auto x : a) {
+        cout << x << ' ';
+    }
+
+}
+int32_t main()
+{
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc = 1;
+    // int t;cin>>t;while(t--)
+    {
+        solve(tc);
+        tc++;
+    }
 }
